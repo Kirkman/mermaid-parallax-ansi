0565a14d48c89897fe3c2432399cf03af8a36eac
Josh Renaud <kirkman@admins-Mac-Pro.local>
2015-10-30 13:17:35 -0500

    Initial commit, take 2
    let’s actually include the files this time.

---
+++ bg-scroll-animation.js
@@ -0,0 +1,397 @@
+load("sbbsdefs.js");
+load("frame.js");
+load("sprite.js");
+load(js.exec_dir + "frame-transitions.js");
+load(js.exec_dir + "helper-functions.js");
+
+// GLOBAL FRAME VARIABLES
+var player = new Object();
+var bgFrame, bgFrame1, bgFrame2, bgFrame3, bgFrame4, fgFrame;
+var bgFrameArray = [];
+
+// COLOR CODES
+var lowWhite = 'NW0';
+var highWhite = 'HW0';
+var lowCyan = 'NC0';
+var highCyan = 'HC0';
+var highBlack = 'HK0';
+var highYellowDarkBlue = 'HY4';
+var highWhiteDarkCyan = 'HW6';
+
+
+// Make an empty frame completely transparent
+function clearFrame(theFrame) {
+	var x, y, xl, yl;
+	xl = theFrame.data.length;
+	for (x=0; x<xl; x++) {
+		yl = theFrame.data[x].length;
+		for (y=0; y<yl; y++) {
+			theFrame.data[x][y].ch = undefined;
+			theFrame.data[x][y].attr = undefined;
+		}
+	}
+}
+
+
+// Fill an empty frame with particular character
+function fillFrame(theFrame,theChar,theAttr) {
+	var x, y, xl, yl;
+	xl = theFrame.data.length;
+	for (x=0; x<xl; x++) {
+		yl = theFrame.data[x].length;
+		for (y=0; y<yl; y++) {
+			theFrame.data[x][y].ch = theChar;
+			theFrame.data[x][y].attr = theAttr;
+		}
+	}
+}
+
+// Mask any frame. Turns all instances of characters with given attributes transparent.
+function maskFrame(theFrame,maskChar,maskAttr) {
+	var x, y, xl, yl;
+	xl = theFrame.data.length;
+	for (x=0; x<xl; x++) {
+		yl = theFrame.data[x].length;
+		for (y=0; y<yl; y++) {
+			var theChar = theFrame.data[x][y];
+			// If this character matches, then delete the character attributes 
+			// in order to make it act as transparent.
+			if (theChar.ch == maskChar && theChar.attr == maskAttr) {
+				theFrame.data[x][y].ch = undefined;
+				theFrame.data[x][y].attr = undefined;
+			} 
+		}
+	}
+}
+
+
+function makeBg() {
+	bgFrame = new Frame(1, 1, 80, 24, BG_BLACK);
+
+	bgFrame4 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame3 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame2 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame1 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	fgFrame  = new Frame(1, 1, 80, 24, BG_BLACK, bgFrame);
+	msgFrame = new Frame(1, 1, 80, 24, BG_BLACK, bgFrame);
+
+	bgFrame4.load(js.exec_dir + '/graphics/background4.bin', 160, 23);
+	bgFrame3.load(js.exec_dir + '/graphics/background3.bin', 160, 23);
+	bgFrame2.load(js.exec_dir + '/graphics/background2.bin', 160, 23);
+	bgFrame1.load(js.exec_dir + '/graphics/background1.bin', 160, 23);
+	player.sprite = new Sprite.Profile("mermaid", bgFrame, 18, 8, 'e', 'stand');
+	fgFrame.load(js.exec_dir + '/graphics/title1.bin', 80, 24);
+
+	bgFrameArray = [
+		bgFrame4,
+		bgFrame3,
+		bgFrame2,
+		bgFrame1
+	];
+	// initialize each layer with the same properties
+	for (var b=0; b<bgFrameArray.length; b++) {
+		// FORMAT: maskFrame( frame, maskChar, maskAttr )
+		// ascii(219) = solid block
+		maskFrame( bgFrameArray[b], ascii(219), LIGHTRED );
+		bgFrameArray[b].h_scroll = true;
+		bgFrameArray[b].v_scroll = false;
+		bgFrameArray[b].transparent = true;
+	}
+	maskFrame( player.sprite.frame, ascii(219), MAGENTA );
+
+	// assign parallax ratio for each layer
+	bgFrame1.parallaxRatio = 3;
+	bgFrame2.parallaxRatio = 2;
+	bgFrame3.parallaxRatio = 1;
+	bgFrame4.parallaxRatio = 0.5;
+
+	// open all background layers
+	bgFrame.open();
+	for (var b=0; b<bgFrameArray.length; b++) {
+		bgFrameArray[b].open();
+	}
+
+	//player.sprite.frame.draw();
+
+	fgFrame.transparent = true;
+ 	//fillFrame(fgFrame,ascii(219), BLACK);
+	fgFrame.top();
+	fgFrame.open();
+
+	msgFrame.gotoxy(0,8);
+	msgFrame.center(highBlack + 'This demo will output a lot of ANSI.');
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'It usually takes about ' + lowWhite + '1-2 minutes' + highBlack + ' to display.');
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'A few seconds after the demo finishes, you will be returned to the BBS.');
+	msgFrame.crlf();
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'Note: Latency issues may cause the demo to become garbled.');
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'If you experience this, you may want to try the "user-controlled" demo instead.');
+	msgFrame.crlf();
+	msgFrame.crlf();
+	msgFrame.center(lowWhite + 'HIT ' + highWhite + '[ENTER]' + lowWhite + ' TO BEGIN THE DEMO.');
+
+	msgFrame.top();
+	msgFrame.draw();
+	msgFrame.open();
+	var userInput;
+	while ( ascii(userInput) != 13 ) {
+		userInput = console.getkey(K_UPPER | K_NOCRLF);
+		// do nothing;
+	}
+	msgFrame.close();
+	msgFrame.delete();
+
+}
+
+
+
+
+
+function play() {
+	var userInput = '';
+	var beat = 1;
+	var xl = fgFrame.width;
+	var yl = fgFrame.height;
+	var x = xl-1;
+	var y = 0;
+	var p = 0;
+	// screenshot counter
+	var ss = 0;
+	// screenshot switch. Change this to false to disable.
+	var screenShot = false;
+	// main animation
+	var fr = 0;
+	var numFrames = 192;
+	var pixelArray = [];
+	// Best wipeSizes are evenly divisible into 80: 1, 2, 4, 5, 8, 10
+	var wipeSize = 6; 
+	var wipeGradientStep = parseInt( (yl-2) / wipeSize );
+	var wipeFrLen = parseInt( xl / wipeSize );
+	// opening title screen
+	var numTitleFrames = 16;
+	// end credits screen
+	var creditsFr = 0;
+	var numCreditsFrames = 8;
+
+	// ==========================================
+	// MERMAID ANIMATION (with wipe intro/outtro)
+	// ==========================================
+
+	for ( fr=0; fr<numFrames; fr++ ) {
+		// Record what time we began the loop 
+		var beginFrameDraw = system.timer;
+
+		// reset beat counter if necessary
+		if (beat > 4) {
+			beat = 1;
+		}
+
+		var direction = 1;
+		var amt = 1;
+
+		// - - - - - - - - - - - - - - - - - - - - -
+		// TITLE SCREEN ANIMATION
+		// - - - - - - - - - - - - - - - - - - - - -
+
+		if ( fr < numTitleFrames ) {
+			fgFrame.load(js.exec_dir + '/graphics/title' + beat + '.bin', 80, 24);
+			fgFrame.draw();
+			if (screenShot) {
+				bgFrame.screenShot(js.exec_dir + "/screenshots/mermaid-" + ss + ".bin", false);
+				ss++;
+			}
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - -
+		// ANIMATE THE SPRITE
+		// - - - - - - - - - - - - - - - - - - - - -
+
+		if ( fr >= numTitleFrames ) {
+			// On first two beats, should be in WALK position
+			if (beat == 1 || beat == 2 ) {
+				player.sprite.position = 'walk';
+				// Iterate over all background layers and scroll them
+				for (var b=0; b<bgFrameArray.length; b++) {
+					var totalMove = 0;
+					// Calculate how far this background layer should scroll
+					totalMove = amt * direction * bgFrameArray[b].parallaxRatio;
+					// Ensure layers with fractional ratios only scroll every other beat
+					if ( (totalMove >= 1) || (beat == 1) ) {
+						bgFrameArray[b].scrollCircular(totalMove,0);
+					}
+				}
+			}
+
+			// On first two beats, should be in STAND position
+			else if (beat == 3 || beat == 4 ) {
+				player.sprite.position = 'stand';
+				// Iterate over all background layers and scroll them
+				for (var b=0; b<bgFrameArray.length; b++) {
+					var totalMove = 0;
+					// Calculate how far this background layer should scroll
+					totalMove = amt * direction * bgFrameArray[b].parallaxRatio;
+					// Ensure layers with fractional ratios only scroll every other beat
+					if ( (totalMove >= 1) || (beat == 3) ) {
+						bgFrameArray[b].scrollCircular(totalMove,0);
+					}
+				}
+			}
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - -
+		// ANIMATE THE WIPE
+		// - - - - - - - - - - - - - - - - - - - - -
+
+		if ( (fr > numTitleFrames) && (fr < (numTitleFrames + wipeFrLen)) ) {
+			// A wipe is a slow painting (or erasing) across the screen, 
+			// with the trailing edge consisting of gradient, wipeSize cols wide. 
+			// Each column in the gradient will paint successively fewer characters.
+
+			fgFrame.load(js.exec_dir + '/graphics/title' + beat + '.bin', 80, 24);
+
+			// Clear out the columns from the LAST wipe gradient. 
+			if ( x < ( xl - (wipeSize) ) ) {
+				var cleanupSize = xl-x;
+				for (var oldx=xl-cleanupSize; oldx<xl; oldx++) {
+					for (y=0; y<yl; y++) {
+						fgFrame.data[y][oldx].ch = undefined;
+						fgFrame.data[y][oldx].attr = undefined;
+					}
+				}
+			}
+			// Generate the new wipe gradient.
+			for (p=0;p<(wipeSize); p++) {
+				pixelArray[p] = [];
+				for (y=0; y<yl; y++) {
+					pixelArray[p].push(y);
+				}
+				//debug( '----------------'  );
+				//debug( 'p: ' + p + ' | x: ' + x  );
+				while( pixelArray[p].length > ( (wipeGradientStep * p) + 2 ) ) {
+					//debug( 'pixelArray: ' + pixelArray );
+					//debug( 'pixelArray[p]: ' + pixelArray[p] );
+					var randomIndex = Math.floor(Math.random() * pixelArray[p].length);
+					var randomPixel = pixelArray[p].splice(randomIndex, 1);
+					//debug( 'randomIndex: ' + randomIndex + ' | randomPixel: ' + randomPixel  );
+					fgFrame.data[randomPixel][x-p].ch = undefined;
+					fgFrame.data[randomPixel][x-p].attr = undefined;
+				}
+			}
+			x = x - (wipeSize);
+		}
+		// Clear fgFrame after first wipe, and reset X coordinate
+ 		else if (fr == (numTitleFrames + wipeFrLen) ) {
+			fgFrame.clear();
+			// reset the wipe x coordinate, to be reused on end wipe.
+			x=xl-1;
+		}
+		// Clear screen completely after end wipe
+ 		if ( fr == numFrames-1 ) {
+			fgFrame.delete();
+			fgFrame  = new Frame(1, 1, 80, 24, BG_BLACK, bgFrame);
+			fgFrame.open();
+		}
+		// End wipe
+		else if (fr >= (numFrames - wipeFrLen) - 1 ) {
+// 	  		debug( 'Final wipe. Frame:' + fr + ' | x:' + x );
+// 	  		debug( 'xl:' + xl + ' | x:' + x );
+
+			// A wipe is a slow painting across the screen, with the trailing
+			// edge consisting of gradient of (wipeSize) columns. 
+			// Each column will have successively fewer characters.
+			
+			// Clear out the columns from the LAST wipe gradient. 
+			if ( x < ( xl - (wipeSize) ) ) {
+				var cleanupSize = wipeSize;
+				for (c = 0; c<cleanupSize; c++) {
+					var oldx = (x + cleanupSize) - c;
+					for (y=0; y<yl; y++) {
+						fgFrame.data[y][oldx].ch = ascii(214);
+						fgFrame.data[y][oldx].attr = BLACK;
+					}
+				}
+			}
+			// Generate the new wipe gradient.
+			for (p=0;p<(wipeSize); p++) {
+				pixelArray[p] = [];
+				for (y=0; y<yl; y++) {
+					pixelArray[p].push(y);
+				}
+				while( pixelArray[p].length > ( (wipeGradientStep * p) + 2 ) ) {
+					var randomIndex = Math.floor(Math.random() * pixelArray[p].length);
+					var randomPixel = pixelArray[p].splice(randomIndex, 1);
+					// ascii(219) = solid block; 5 = Magenta
+					fgFrame.data[randomPixel][x-p].ch = ascii(214);
+					fgFrame.data[randomPixel][x-p].attr = BLACK;
+				}
+			}
+			x = x - (wipeSize);
+		}
+
+		// - - - - - - - - - - - - - - - - - - - - -
+		// RENDER EVERYTHING TO SCREEN
+		// - - - - - - - - - - - - - - - - - - - - -
+
+		player.sprite.frame.draw();
+		Sprite.cycle();
+		fgFrame.top();
+		fgFrame.refresh();
+		fgFrame.draw();
+		fgFrame.cycle();
+// 		if (beat == 1) { mswait(500); }
+
+		// Record what time we finished drawing
+		var endFrameDraw = system.timer;
+
+		// increment beat counter
+		beat++;
+		if (screenShot) {
+			bgFrame.screenShot(js.exec_dir + "/screenshots/mermaid-" + ss + ".bin", false);
+			ss++;
+		}
+//  		debug( 'Frame:' + fr );
+//  		debug( 'Position:' + player.sprite.position );
+//  		debug( 'Drawing time: ' + ( endFrameDraw - beginFrameDraw ) );
+
+	} // end for loop
+
+
+	// ============================
+	// END CREDITS SCREEN ANIMATION
+	// ============================
+
+	for (creditsFr=0; creditsFr<numCreditsFrames; creditsFr++) {
+		fgFrame.load(js.exec_dir + '/graphics/credits' + (creditsFr + 1) + '.bin', 80, 24);
+		fgFrame.draw();
+		if (screenShot) {
+			bgFrame.screenShot(js.exec_dir + "/screenshots/mermaid-" + ss + ".bin", false);
+			ss++;
+		}
+		mswait(200);
+	}
+
+	mswait(3000);
+// 	while ( ascii(userInput) != 13 ) {
+// 		userInput = console.getkey(K_UPPER | K_NOCRLF);
+// 	}
+
+} // play()
+
+
+
+function main_loop() {
+	makeBg();	
+	bgFrame.cycle();
+	play();
+	exit();
+}
+
+
+
+// Run the animation
+main_loop();
+
+
---
+++ bg-scroll.js
@@ -0,0 +1,256 @@
+load("sbbsdefs.js");
+load("json-client.js");
+load("event-timer.js");
+load("frame.js");
+load("layout.js");
+load("sprite.js");
+
+load(js.exec_dir + "frame-transitions.js");
+load(js.exec_dir + "helper-functions.js");
+
+
+var serverIni;
+var players = [];
+var player = new Object();
+var bgFrame, bgFrame1, bgFrame2, bgFrame3, bgFrame4, msgFrame;
+var stepCount = 1;
+var places = [];
+var bgFrameArray = [];
+
+
+// COLOR CODES
+var lowWhite = 'NW0';
+var highWhite = 'HW0';
+var lowCyan = 'NC0';
+var highCyan = 'HC0';
+var highBlack = 'HK0';
+var highYellowDarkBlue = 'HY4';
+var highWhiteDarkCyan = 'HW6'; 
+
+
+function main_loop() {
+	makeBg();	
+	var is_playing = true;
+	var time;
+	bgFrame.cycle();
+	gamePlay();
+}
+
+
+
+
+function makeBg() {
+	bgFrame = new Frame(1, 1, 80, 24, BG_BLACK);
+
+	bgFrame4 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame3 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame2 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	bgFrame1 = new Frame(1, 1, 80, 24, undefined, bgFrame);
+	msgFrame = new Frame(1, 1, 80, 24, BG_BLACK, bgFrame);
+
+	bgFrame4.load(js.exec_dir + '/graphics/background4.bin', 160, 23);
+	bgFrame3.load(js.exec_dir + '/graphics/background3.bin', 160, 23);
+	bgFrame2.load(js.exec_dir + '/graphics/background2.bin', 160, 23);
+	bgFrame1.load(js.exec_dir + '/graphics/background1.bin', 160, 23);
+
+	// FORMAT: maskFrame( frame, maskChar, maskAttr )
+	// ascii(219) = solid block
+	maskFrame( bgFrame1, ascii(219), LIGHTRED );
+	maskFrame( bgFrame2, ascii(219), LIGHTRED );
+	maskFrame( bgFrame3, ascii(219), LIGHTRED );
+	maskFrame( bgFrame4, ascii(219), LIGHTRED );
+
+	bgFrame1.h_scroll = true;
+	bgFrame1.v_scroll = false;
+	bgFrame1.transparent = true;
+	bgFrame2.h_scroll = true;
+	bgFrame2.v_scroll = false;
+	bgFrame2.transparent = true;
+	bgFrame3.h_scroll = true;
+	bgFrame3.v_scroll = false;
+	bgFrame3.transparent = true;
+	bgFrame4.h_scroll = true;
+	bgFrame4.v_scroll = false;
+	bgFrame4.transparent = true;
+
+	bgFrame1.parallaxRatio = 3;
+	bgFrame2.parallaxRatio = 2;
+	bgFrame3.parallaxRatio = 1;
+	bgFrame4.parallaxRatio = 0.5;
+
+	bgFrameArray = [
+		bgFrame4,
+		bgFrame3,
+		bgFrame2,
+		bgFrame1
+	];
+
+	bgFrame.open();
+	for (var b=0; b<bgFrameArray.length; b++) {
+		bgFrameArray[b].open();
+	}
+
+	msgFrame.gotoxy(0,8);
+	msgFrame.center(highBlack + 'BEWARE: This demo will output a lot of ANSI.');
+	msgFrame.crlf();
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'Use your ' + lowWhite + 'left-' + highBlack + ' and ' + lowWhite + 'right-arrow' + highBlack + ' keys to move the mermaid.');
+	msgFrame.crlf();
+	msgFrame.center(highBlack + 'You can exit the demo any time by hitting ' + lowWhite + '[ESC]' + highBlack + ' or ' + lowWhite + '[Q]' + highBlack + '.');
+	msgFrame.crlf();
+	msgFrame.crlf();
+	msgFrame.center(lowWhite + 'HIT ' + highWhite + '[ENTER]' + lowWhite + ' TO BEGIN THE DEMO.');
+
+	msgFrame.top();
+	msgFrame.draw();
+	msgFrame.open();
+	var userInput;
+	while ( ascii(userInput) != 13 ) {
+		userInput = console.getkey(K_UPPER | K_NOCRLF);
+		// do nothing;
+	}
+	msgFrame.close();
+	msgFrame.delete();
+
+
+}
+
+// this version makes an empty frame completely transparent
+function clearFrame(theFrame) {
+	var x, y, xl, yl;
+	xl = theFrame.data.length;
+	for (x=0; x<xl; x++) {
+		yl = theFrame.data[x].length;
+		for (y=0; y<yl; y++) {
+			theFrame.data[x][y].ch = undefined;
+			theFrame.data[x][y].attr = undefined;
+		}
+	}
+}
+
+
+// this version masks the entire sprite up front.
+function maskFrame(theFrame,maskChar,maskAttr) {
+	var x, y, xl, yl;
+	xl = theFrame.data.length;
+	for (x=0; x<xl; x++) {
+		yl = theFrame.data[x].length;
+		for (y=0; y<yl; y++) {
+			var theChar = theFrame.data[x][y];
+			//debug(theChar);
+			// If this character is an empty black space, 
+			// then delete the character attributes in order
+			// to make it act as transparent.
+			if (theChar.ch == maskChar && theChar.attr == maskAttr) {
+//			if (theChar.ch == ascii(219) && theChar.attr == 514) {
+				theFrame.data[x][y].ch = undefined;
+				theFrame.data[x][y].attr = undefined;
+			} 
+		}
+	}
+}
+
+
+
+
+// This is totally not finished yet.
+
+function gamePlay() {
+	player.sprite = new Sprite.Profile("mermaid", bgFrame, 18, 8, 'e', 'stand');
+
+	// FORMAT: maskFrame( frame, maskChar, maskAttr )
+	// ascii(219) = solid block
+	maskFrame( player.sprite.frame, ascii(219), MAGENTA );
+	player.sprite.frame.draw();
+
+	var userInput = '';
+	while( ascii(userInput) != 27 && ascii(userInput) != 81 ) {
+		userInput = console.getkey(K_UPPER | K_NOCRLF);
+		//player.sprite.getcmd(userInput);
+// 		msgFrame.clear();
+// 		msgFrame.close();
+// 		msgFrame.open();
+//		msgFrame.center('offset  ||  X: ' + bgFrame.offset['x'] + '  Y: ' + bgFrame.offset['y']);
+// 		msgFrame.center('lastMove: ' + player.sprite.lastMove + '  Now: ' + system.timer );
+// 		msgFrame.crlf();
+// 		msgFrame.center('bearing: ' + player.sprite.bearing + '  canMove: ' + player.sprite.canMove() );
+		if ( player.sprite.canMove() ) {
+			var beginTime = system.timer;
+			var direction = 1;
+			var amt = 1;
+			if ( userInput == KEY_LEFT ) {
+				direction = -1;
+				if (player.sprite.bearing != 'w') {
+					player.sprite.turnTo('w');
+					amt = 0;
+				}
+			}
+			else if ( userInput == KEY_RIGHT ) {
+				if (player.sprite.bearing != 'e') {
+					player.sprite.turnTo('e');
+					amt = 0;
+				}
+			}
+
+			if ( userInput == KEY_LEFT || KEY_RIGHT ) {
+				player.sprite.lastMove = system.timer;
+				if ( player.sprite.position == 'walk' ) {
+					player.sprite.position = 'stand';
+					for (var b=0; b<bgFrameArray.length; b++) {
+						var totalMove = 0;
+						totalMove = amt * direction * bgFrameArray[b].parallaxRatio;
+						if (totalMove >= 1 || totalMove <= -1) {
+							bgFrameArray[b].scrollCircular(totalMove,0);
+						}
+					}
+				}
+				else if ( player.sprite.position == 'stand' ) {
+					player.sprite.position = 'walk';
+					for (var b=0; b<bgFrameArray.length; b++) {
+						var totalMove = 0;
+						totalMove = amt * direction * bgFrameArray[b].parallaxRatio;
+						bgFrameArray[b].scrollCircular(totalMove,0);
+					}
+				}
+				player.sprite.frame.draw();
+			}
+
+			Sprite.cycle();
+			var endTime = system.timer;
+// 			debug( '--------------------' );
+// 			debug( 'BEGIN:' + beginTime );
+// 			debug( 'END:' + endTime );
+// 			debug( 'POSITION WHEN DRAWN:' + player.sprite.position );
+// 			debug( 'TOTAL DRAWING TIME:' +  (endTime-beginTime) );
+
+//			debug('BEGIN DEBUG BGFRAME');
+// 			debugFrame(bgFrame);
+//			bgFrame.screenShot(js.exec_dir + "frame-layers" + endTime + ".bin", false);
+		} // if player.sprite.canMove
+	} // end while
+
+
+
+} // gamePlay()
+
+
+
+
+
+
+
+
+// Run the game's main loop until it returns.
+main_loop();
+
+
+
+		
+	
+
+
+
+
+
+
+
